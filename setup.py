fba51ea0cb4587bf27e8f1b857a99825f0b97004
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-03-12 09:13:47 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+
+setup(
+    name="pytorch_templates",
+    version=0.1, 
+    description="A package to create pytorch projects quickly",
+    author="Aneesh Aparajit G",
+    author_email="aneeshaparajit.g2002@gmail.com",
+    install_requires=[required_packages],
+    packages=find_namespace_packages()
+)
